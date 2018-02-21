5b7b19b8c4c9ffbbfe9282510e958b6b88682d5e
C.T.Raines.III <c.t.raines.iii@gmail.com>
2018-02-21 13:33:57 -0700

    Clifford

--- src/main/java/Limbo/Game.java
+++ src/main/java/Limbo/Game.java
@@ -3,12 +3,21 @@ package Limbo;
 import GameState.GameStateController;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.layout.Pane;
 
-public class Game implements EventHandler<Event> {
+public class Game extends Pane implements EventHandler<Event>, Runnable {
 
     private GameStateController gsc;
 
     public void handle(Event event) {
         gsc.handle(event);
+    }
+
+    public void run() {
+
+    }
+
+    public void init(){
+
     }
 }
--- src/main/java/Limbo/Main.java
+++ src/main/java/Limbo/Main.java
@@ -1,7 +1,25 @@
 package Limbo;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(Main.class,args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/samuel.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = primaryStage;
+        stage.setTitle("Single Smack Samuel paint 2000!!!");
+        stage.setScene(scene);
+        stage.show();
+        Game controller = loader.getController();
+        controller.init();
     }
 }
