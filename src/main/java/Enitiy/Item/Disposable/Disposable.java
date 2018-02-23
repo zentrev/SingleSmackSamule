48b19cf55705412fe3426e23149fe2aff374f25c
FestiveArt <jachristensen@student.neumont.edu>
2018-02-23 14:07:46 -0700

    Jason

--- src/main/java/Enitiy/Item/Disposable/Disposable.java
+++ src/main/java/Enitiy/Item/Disposable/Disposable.java
@@ -3,4 +3,17 @@ package Enitiy.Item.Disposable;
 import Enitiy.Item.Item;
 
 public class Disposable extends Item {
+
+
+    public void update(){
+
+    }
+
+    public void draw(){
+
+    }
+
+    public void  comitEvent(){
+
+    }
 }
--- src/main/java/Enitiy/Item/Usable/Usable.java
+++ src/main/java/Enitiy/Item/Usable/Usable.java
@@ -3,4 +3,19 @@ package Enitiy.Item.Usable;
 import Enitiy.Item.Item;
 
 public class Usable extends Item {
+
+    private int useLocation;
+
+    public void update(){
+
+    }
+
+    public void draw(){
+
+    }
+
+    public void  comitEvent(){
+
+    }
+
 }
