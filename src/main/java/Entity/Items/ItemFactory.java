80c90e7186a82ddd8bf92a735c5174f331604ecd
Jose Arteaga <arteaga.jose20299@gmail.com>
2018-02-28 14:45:58 -0700

    Jose

---
+++ src/main/java/Entity/Items/ItemFactory.java
@@ -0,0 +1,17 @@
+package Entity.Items;
+
+import Entity.Monster.Monster;
+import Entity.Monster.mon1;
+import GameStateManager.Room;
+import TileMap.Tile;
+
+public class ItemFactory {
+    public Item getItem(Tile[][] tileMap, int itemId, int x, int y){
+        switch(itemId){
+            case 1:
+                return new Item1(tileMap, x* Room.tileSize, y*Room.tileSize);
+            default:
+                return null;
+        }
+    }
+}
