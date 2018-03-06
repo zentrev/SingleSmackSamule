763332058337720864794d2d154a077216f4efef
C.T.Raines.III <c.t.raines.iii@gmail.com>
2018-03-05 17:57:08 -0700

    Clifford

--- src/main/java/GameStateManager/RoomState.java
+++ src/main/java/GameStateManager/RoomState.java
@@ -14,7 +14,7 @@ public class RoomState extends GameState {
 
     public static Samuel sam;
 
-    private static final int NUMBEROFROOMS = 6;
+    private static final int NUMBEROFROOMS = 7;
 
     public RoomState(GameStateManager gsm) {
         super(gsm);
