b753fd06242ef58de379246556aa200cc06df254
FestiveArt <jachristensen@student.neumont.edu>
2018-03-06 14:55:20 -0700

    Jason

--- src/main/java/GameStateManager/GameState.java
+++ src/main/java/GameStateManager/GameState.java
@@ -7,12 +7,34 @@ abstract public class GameState {
 
     protected GameStateManager gsm;
 
+    /**
+     * checks the initial, does the update, draws out the game and handles all the events or items.
+     * @param gsm
+     */
     public GameState(GameStateManager gsm){
         this.gsm = gsm;
     }
 
+    /**
+     * inits the game
+     * @param gamePane
+     */
     public abstract void init(Pane gamePane);
+
+    /**
+     * updates the game.
+     */
     public abstract void update();
+
+    /**
+     * draws out the maps
+     * @param gamePane
+     */
     public abstract void draw(Pane gamePane);
+
+    /**
+     * handles the events.
+     * @param event
+     */
     public abstract void handle(Event event);
 }
